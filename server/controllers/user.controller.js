c843b73c09fbb6e4eda4928940212f7d9472ba07
Acearoni <awoodson.contact@gmail.com>
2025-02-17 20:27:52 -0600

    Login, Login implemented, cookie clear tested and confirmed

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -15,14 +15,39 @@ module.exports = {
                 const newUser = await User.create(req.body)
                 // console.log(newUser)
                 //!GENERATE JSON WEBTOKEN!//
-                const userToken = jwt.sign({id:newUser._id, email: newUser.email}, secretKey, {expiresIn: '2h'})
+                const userToken = jwt.sign({ id: newUser._id, email: newUser.email }, secretKey, { expiresIn: '2h' })
                 // console.log(userToken)
-                res.status(201).cookie('userToken', userToken, {httpOnly:true}).json(newUser)
+                res.status(201).cookie('userToken', userToken, { httpOnly: true }).json(newUser)
             }
         }
         catch (err) {
             res.status(500).json({ error: err })
         }
+    },
+    loginUser: async (req, res) => {
+        try {
+            //Check if email matches
+            const potentialUser = await User.findOne({ email: req.body.email })
+            if (potentialUser) {
+                //if it does, check if the password hash match
+                const passwordsMatch = await bcrypt.compare(req.body.password, potentialUser.password)
+                if (passwordsMatch) {
+                    const userToken = jwt.sign({ id: potentialUser._id, email: potentialUser.email }, secretKey, { expiresIn: '2h' })
+                    res.status(201).cookie('userToken', userToken, { httpOnly: true }).json(potentialUser)
+                } else {
+                    res.status(400).json({message: 'Invalid email / password'})
+                }
+            } else {
+                res.status(400).json({message: 'invalid email / password'})
+            }
+        }
+        catch (err) {
+            res.status(500).json({ error: err })
+        }
+    },
+    logoutUser: (req, res) => {
+        res.clearCookie('userToken')
+        res.status(200).json({message: 'User logged out'})
     }
 }
 
