ecbf63f23ccd17c491f04965960bed941c7286d7
Acearoni <awoodson.contact@gmail.com>
2025-02-20 20:31:19 -0600

    config changed for cookie clear on logout

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 const cookieParser = require('cookie-parser') //Imports cookie parser to send cookies to client
 
 app.use(express.json(), express.urlencoded({extended: true}));
-app.use(cors({credentials:true, origin:'http://localhost:5174'}));
+app.use(cors({credentials:true, origin:'http://localhost:5173'}));
 app.use(cookieParser())//use cookieParser
 require('./config/mongoose.config')
 
